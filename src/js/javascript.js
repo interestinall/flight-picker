e1384f800d5c48822da4e260234cea27628a0fba
Justin LeFurjah <lefurjah@gmail.com>
2015-03-02 11:25:03 +0100

    jquery functionality added to radios

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -8,4 +8,31 @@ $(function() {
     $( "#children").selectmenu({
     	width: 85
     });
+    $( "#dd").datepicker();
+    $( "#rd").datepicker();
   });
+
+ $(document).ready(function(){
+ $( "#radio2" ).change(function() {
+      $( ".r-date" ).hide();
+      $( ".multiple" ).addClass( "hide" );
+      console.log("radio two triggered");
+    });
+
+ $( "#radio1" ).change(function() {
+      $( ".r-date" ).show();
+       $( ".multiple" ).addClass( "hide" );
+      console.log("radio one triggered");
+    });
+
+ $( "#radio3" ).change(function() {
+      $( ".multiple" ).removeClass("hide");
+      $( ".r-date" ).hide();
+      $(".wrapper").removeClass("wrapper").addClass("wrapper-big");
+      $(".flight-picker").removeClass("flight-picker").addClass("flight-picker-big");
+
+
+      console.log("radio three triggered");
+    });  
+
+ });
